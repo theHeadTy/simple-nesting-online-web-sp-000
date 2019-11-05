c82e8f39808e78cd4e3b50850ce9733dd937863a
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-05 06:16:19 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,15 +57,17 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
+  new = { 
+    :yukihiro_matsumoto => {
+      :known_for => 'Ruby',
+      :languages => ['LISP', 'C']
+    }
+  }
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -80,6 +84,7 @@ def adding_matz
         }
      }
 
+  new.merge!(programmer_hash)
     
 end
 
@@ -102,11 +107,16 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay].update({
+    :known_for => 'GUI'
+  })
+  
+  programmer_hash
+
 end
 
 def adding_to_dennis
-	programmer_hash = 
+ programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -121,5 +131,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     
+     # expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
+  
+  programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+  programmer_hash
+  
 end
